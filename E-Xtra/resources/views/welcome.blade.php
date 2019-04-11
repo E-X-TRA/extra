348c4d531e89bcc70e284a732b58bbc9eed6eb00
putra <raihanputraramd@gmail.com>
2019-04-11 09:46:57 +0700

    ganti warna lagi

--- E-Xtra/resources/views/welcome.blade.php
+++ E-Xtra/resources/views/welcome.blade.php
@@ -23,28 +23,64 @@
                       background-position: center;
                       background-repeat: no-repeat;
                       background-size: 350px;
+                      background-color: #cdedf6
             }
             .timmana {
                     font-family: 'Timmana';
                     font-size: 175px;
                     margin-top: 1.1em;
-                    color: white
+                    color: #cdedf6;
+            }
+            .warna{
+                background-color: #006496;
             }
         </style>
     </head>
     <body>
       <div class="parallax"></div>
-      <div style="height: 42em;" class="bg-secondary">
+
+    <div style="height: 42em;" class="warna">
         <div class="container-fluid">
             <div class="row">
                 <div class="col"></div>
                 <div class="col">
                     <div class="text-center">
                         <h1 class="timmana">Extra</h1>
-                        <p class="text-capitalize">dimana kami memmbuat semua hal lebih mudah</p>
+                        <h3 class="text-capitalize" style="color: #cdedf6">dimana kami membuat semua hal lebih mudah</h3>
                     </div>
                 </div>
                 <div class="col"></div>
+            </div>
+        </div>
+        <div class="container-fluid">
+            <div class="row">
+                <div class="col">
+                    <div class="card">
+                        <img class="card-img-top" src="holder.js/100x180/" alt="">
+                        <div class="card-body">
+                            <h4 class="card-title">Title</h4>
+                            <p class="card-text">Text</p>
+                        </div>
+                    </div>
+                </div>
+                <div class="col">
+                    <div class="card">
+                        <img class="card-img-top" src="holder.js/100x180/" alt="">
+                        <div class="card-body">
+                            <h4 class="card-title">Title</h4>
+                            <p class="card-text">Text</p>
+                        </div>
+                    </div>
+                </div>
+                <div class="col">
+                    <div class="card">
+                        <img class="card-img-top" src="holder.js/100x180/" alt="">
+                        <div class="card-body">
+                            <h4 class="card-title">Title</h4>
+                            <p class="card-text">Text</p>
+                        </div>
+                    </div>
+                </div>
             </div>
         </div>
     </div>
