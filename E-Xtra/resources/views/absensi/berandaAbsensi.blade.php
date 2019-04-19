0743017b5d4595e792aa928fc0aa65d5671b5834
alvarodwi <varoalvaro493@gmail.com>
2019-04-19 12:44:58 +0700

    Revert "urang teh tadi nyobain di merge tapi malah jadi kosong si filenya"
    This reverts commit 77f4c59de186e6f21b93dcb1e6a99fe4812b7fa7.

--- E-Xtra/resources/views/absensi/berandaAbsensi.blade.php
+++ E-Xtra/resources/views/absensi/berandaAbsensi.blade.php
@@ -3,8 +3,19 @@
 <head>
 	<title>Absensi</title>
 
-</head>
+	<meta charset="UTF-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<link rel="stylesheet" type="text/css" href="{{ asset('css/bootstrap.css')}}">
+    <link rel="stylesheet" href="{{ asset('/css/bootstrap.min.css')}}">
 
+    <style>
+        body{
+            background-color:
+        }
+    </style>
+
+</head>
+<body class="bg-grd" style="background-color: #042a2b">
 	@if(session('success'))
 	<div class="alert alert-success">
 		{{ session('success') }}
@@ -15,15 +26,38 @@
 		{{ session('error') }}
 	</div>
 	@endif
-
 	@php
 		use Carbon\Carbon;
 
 		$today = Carbon::today();
 	@endphp
+    <div class="container">
+        <div class="row">
+            <div class="col"></div>
+            <div class="col-10">
+                <div class="card" style="margin:210px; padding: 30px;">
+                    <div class="card-body">
+                        <h4 class="card-title text-center">Hari ini, {{ $today->toDateString() }}</h4>
+                        <p class="card-text">Jumlah Anggota : xx <a href="{{ url('/absensi/anggota') }}"">Details</a></p>
+                        <p class="card-text">Jumlah Pertemuan hingga hari ini : xx <a href="{{ url('/absensi/rekapan') }}">Lihat Rekap</a></p>
+                        <a class="btn btn-primary card-link" href="{{url('/absensi/buatKumpul')}}">MULAI</a>
+                    </div>
+                </div>
+            </div>
+            <div class="col"></div>
+        </div>
+    </div>
 
 
 
 
+
+
+
+
+
+    <script type="text/javascript" src="public/js/jquery.min.js"></script>
+     <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js"></script>
+	<script type="text/javascript" src="public/js/bootstrap.min.js"></script>
 </body>
 </html>
