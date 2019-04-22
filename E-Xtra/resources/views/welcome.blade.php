11fabf6a44dedb6309f4b7770ffa364cad2930d8
putra <raihanputraramd@gmail.com>
2019-04-22 08:40:00 +0700

    cuman dignati about us

--- E-Xtra/resources/views/welcome.blade.php
+++ E-Xtra/resources/views/welcome.blade.php
@@ -87,45 +87,7 @@
             </div>
         </div>
     </div>
-    <div class="container-fluid" style="margin-top: 8em;background-color: #1e2749;">
-                <h1 class="ibm text-center">Our Team</h1>
-        <div class="row">
-            <div class="container" style="margin-top: 3em; margin-bottom: 3em;">
-                <div class="row">
-                    <div class="col">
-                        <div class="card" style="width:275px; margin-left: 40px; border-width: 5px;" >
-                            <img class="card-img-top" src="{{ asset('img/paro.jpg') }}" alt="Card image" style="width:100%;">
-                            <div class="card-body">
-                              <h3 class="card-title">Back-end</h3>
-                              <img src="{{ asset('img/back-end.png') }}" alt="Front End image"  class="card-title float-right" width="50px">
-                              <h5 class="card-title">Alvaro Dwi </h5>
-                            </div>
-                        </div>
-                    </div>
-                    <div class="col">
-                        <div class="card" style="width:275px; margin-left: 40px; border-width: 5px;" >
-                            <img class="card-img-top" src="{{ asset('img/ikal.jpg') }}" alt="Card image" style="width:100%;">
-                            <div class="card-body">
-                              <h3 class="card-title">Front-end</h3>
-                              <img src="{{ asset('img/front-end.png') }}" alt="Front End image"  class="card-title float-right" width="50px">
-                              <h5 class="card-title">M. Haikal </h5>
-                            </div>
-                        </div>
-                    </div>
-                    <div class="col">
-                        <div class="card" style="width:275px; margin-left: 40px; border-width: 5px;" >
-                            <img class="card-img-top" src="{{ asset('img/gaming.jpg') }}" alt="Card image" style="width:100%;">
-                            <div class="card-body">
-                              <h3 class="card-title">Front-end</h3>
-                              <img src="{{ asset('img/front-end.png') }}" alt="Front End image"  class="card-title float-right" width="50px">
-                              <h5 class="card-title">Raihan Putra </h5>
-                            </div>
-                        </div>
-                    </div>
-                </div>
-            </div>
-        </div>
-    </div>
+
 
 
 
