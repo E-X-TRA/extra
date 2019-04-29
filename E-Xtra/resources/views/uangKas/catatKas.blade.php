2328eb775e48dc363a4c860ea2171b7bd5ea7b13
Raihan Putra Ramadhan <raihanputraramd@gmail.com>
2019-04-30 05:47:06 +0700

    Edit eskul
    urg kasih layout nya buat eskul yang beranda ama catat
    
    yang beranda kenapa grid nya rada bawah nya kosong biar pas modal nya keluar ( coba weh ngke ge ngarti)

--- E-Xtra/resources/views/uangKas/catatKas.blade.php
+++ E-Xtra/resources/views/uangKas/catatKas.blade.php
@@ -8,7 +8,7 @@
 
 <link rel="stylesheet" type="text/css" href="{{ asset('css/app.css')}}">
 <script type="text/javascript" src="https://code.jquery.com/jquery-3.3.0.min.js"></script>
-<body>
+<body style="background-color: #5eb1bf">
 @if(session('success'))
 	<div class="alert alert-success">
 		{{ session('success') }}
@@ -19,31 +19,51 @@
 	<div class="alert alert-error">
 		{{ session('error') }}
 	</div>
-	@endif
-	<form method="POST" action="{{ url('/uangKas') }}">
-		@csrf
-		<h1>Form Catat Uang Kas</h1>
-		<hr>
-		<label>Tanggal Pertemuan</label>
-		<select class="custom-select" name="id_pertemuan">
-		@foreach($pertemuan as $data)
-			<option value="{{ $data->id }}">{{ $data->tanggal }}</option>
-		@endforeach
-		</select>
-		<div class="form-group">
-		<label>Total Uang Kas Masuk</label>
-	    <div class="input-group mb-3">
-	      <div class="input-group-prepend">
-	        <span class="input-group-text">Rp.</span>
-	      </div>
-	      <input type="text" id="currency" class="form-control" name="jumlah_masuk">
-	      <div class="input-group-append">
-	        <span class="input-group-text">.00</span>
-	      </div>
-	    </div>
-	  </div>
-	  <input type="submit" value="SUBMIT">
-	</form>
+    @endif
+
+
+    @extends('layouts.app')
+    <div class="container">
+        @section('content')
+        <div class="row">
+            <div class="col">
+
+            </div>
+            <div class="col-8">
+                    <div class="jumbotron">
+                            <h1 class="display-4">Form Catat Uang Kas</h1>
+                            <hr>
+                             <form action="{{ url('/uangKas') }}" method="POST">
+                                 @csrf
+                                 <div class="form-group">
+                                     <label for="tglPertemuan">Tanggal Pertemuan</label>
+                                     <select class="form-control" name="id_pertemuan" id="tglPertemuan">
+                                         @foreach($pertemuan as $data)
+                                             <option value="{{ $data->id }}">{{ $data->tanggal }}</option>
+                                         @endforeach
+                                         </select>
+                                 </div>
+                                 <div class="form-group">
+                                   <label for="totalUang">Total Uang Kas Masuk</label>
+                                   <div class="input-group">
+                                     <div class="input-group-prepend">
+                                         <span class="input-group-text">Rp.</span>
+                                     </div>
+                                         <input type="text" id="currency" class="form-control" name="jumlah_masuk">
+                                     </div>
+                                   </div>
+                                   <p class="lead">
+                                        <input type="submit" value="SUBMIT" class="btn btn-primary">
+                                   </p>
+                             </form>
+                        </div>
+            </div>
+            <div class="col">
+
+            </div>
+        </div>
+    </div>
+    @endsection
 </body>
 {{-- <script type="text/javascript">
 	var format = function(num){
